f87150858b57a9f83e552c6d31916e5ee14119d8
Ioulia <markou@dtu.dk>
2017-05-02 14:58:12 +0200

    Fix datafiles for v1, add libraries

--- javascript/v1.js
+++ javascript/v1.js
@@ -34,3 +34,108 @@ var svg = d3.select("div#vis1").append("svg")
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// read wounded data from file
+d3.csv('Data/v1/AmericanScores.csv', function(error, data) {
+    if (error) throw error;
+    var gradeNames = d3.keys(data[0]).filter(function(key) {
+        return key !== "Year";
+    });
+    data.forEach(function(d) {
+        d.grades = gradeNames.map(function(name) {
+            return {
+                name: name
+                , value: +d[name]
+            };
+        });
+    });
+    x0.domain(data.map(function(d) {
+        return d.Year;
+    }));
+    x1.domain(gradeNames).rangeRoundBands([0, x0.rangeBand()]);
+    y.domain([0, d3.max(data, function(d) {
+        return d3.max(d.grades, function(d) {
+            return d.value;
+        });
+    })]);
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(3,0)")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 10)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Number of people");
+
+    var year = svg.selectAll(".year")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "year")
+        .attr("transform", function(d) {
+            return "translate(" + x0(d.Year) + ",0)";
+        });
+
+    year.selectAll(".bar")
+        .data(function(d) {
+            return d.grades;
+        })
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("width", x1.rangeBand())
+        .attr("x", function(d) {
+            return x1(d.name);
+        })
+        .attr("y", function(d) {
+            return y(d.value);
+        })
+        .attr("height", function(d) {
+            return height - y(d.value);
+        })
+        .style("fill", function(d) {
+            return color(d.name);
+        });
+
+    d3.selectAll("rect.bar")
+        .on("mouseover", function(d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html("<div>" + d.value + " victims</div>")
+                .style("font-weight", "bold")
+                .style("color", "black")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
+    var legend = svg.selectAll(".legend")
+        .data(gradeNames.slice().reverse())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) {
+            return "translate(80," + i * 20 + ")";
+        });
+    legend.append("rect")
+        .attr("x", width - 760)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+    legend.append("text")
+        .attr("x", width - 770)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) {
+            return d;
+        })
+        .style("fill", color);
